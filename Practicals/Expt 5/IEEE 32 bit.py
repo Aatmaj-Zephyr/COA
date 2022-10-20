7fb0ef575b1f03bf1112ec00c12e157f9a3ccd77
Aatmaj-Zephyr <Aatmaj-Zephyr@users.noreply.github.com>
2022-10-20 06:33:33 +0000

    Automated autoyapf fixes

--- Practicals/Expt 5/IEEE 32 bit.py
+++ Practicals/Expt 5/IEEE 32 bit.py
@@ -7,33 +7,42 @@ def convert_to_binary_integer(num):
     arr.append(1)
     arr = arr[::-1]
     return arr
+
+
 import math
+
+
 def IEEE(num):
-      signbit=[0]
-      if(num<0):
-            signbit=[1]
-            num=num*-1
-      intpart = convert_to_binary_integer(math.floor(num)) #numbers before decimal
-      intpart.pop(0) #remove first 1
-      floatpart= convert_to_binary_float(num-math.floor(num)) #numbers after decimal
-      mantissa=intpart+floatpart
-      exponent= len(intpart)
-      exponent=exponent+127 #bais
-      exponent=convert_to_binary_integer(exponent)
-      print("32 bit representation")
-      print("signbit",signbit)
-      print("exponent",exponent)
-      print("mantissa",mantissa)
-      return signbit+exponent+mantissa
+    signbit = [0]
+    if (num < 0):
+        signbit = [1]
+        num = num * -1
+    intpart = convert_to_binary_integer(
+        math.floor(num))  #numbers before decimal
+    intpart.pop(0)  #remove first 1
+    floatpart = convert_to_binary_float(
+        num - math.floor(num))  #numbers after decimal
+    mantissa = intpart + floatpart
+    exponent = len(intpart)
+    exponent = exponent + 127  #bais
+    exponent = convert_to_binary_integer(exponent)
+    print("32 bit representation")
+    print("signbit", signbit)
+    print("exponent", exponent)
+    print("mantissa", mantissa)
+    return signbit + exponent + mantissa
+
+
 def convert_to_binary_float(num):
-    arr=[]
-    for i in range(0,23):
-        num=num*2
-        if(num>=1):
+    arr = []
+    for i in range(0, 23):
+        num = num * 2
+        if (num >= 1):
             arr.append(1)
-            num=num-1
+            num = num - 1
         else:
             arr.append(0)
     return arr
-    
+
+
 print(IEEE(float(input("Decimal Number "))))

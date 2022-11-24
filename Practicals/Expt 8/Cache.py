3c11bfd4b8e831052dd6320991498f8e9cf8d3ad
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-11-24 19:09:59 +0530

    Create Cache.py

---
+++ Practicals/Expt 8/Cache.py
@@ -0,0 +1,12 @@
+Memorysize=int(input('please enter Memory size in k words  '))*1024
+ 
+cachesize=int(input('please enter cache size in k words '))*1024
+ 
+import math
+tag=math.log2(Memorysize/cachesize)
+block=math.log2(Memorysize)
+word=Memorysize/cachesize /block
+ 
+print("tag ",tag)
+print("block ",block)
+print("word ",word)
